3a305383581c3965bc17a4dd959c4a85ef7e0efc
ncik8 <ncik@me.com>
2026-04-15 16:01:27 +0800

    Use security headers instead of CSP

--- next.config.js
+++ next.config.js
@@ -21,6 +21,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
