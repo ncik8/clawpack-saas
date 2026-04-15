e5f02a8ba9bc4c71ffb8622df7b37a71dfdbc76d
ncik8 <ncik@me.com>
2026-04-15 15:46:38 +0800

    Relax CSP - add connect-src for all social APIs and Supabase

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://**.linkedin.com https://**.licdn.com https://pbs.twimg.com https://abs.twimg.com;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' 'unsafe-hashes'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://**.linkedin.com https://**.licdn.com https://pbs.twimg.com https://abs.twimg.com; connect-src 'self' https://dcyifihwvqxtpypphpef.supabase.co https://*.supabase.co https://api.twitter.com https://upload.twitter.com https://video.bsky.app https://bsky.social https://api.linkedin.com https://*.atproto.com https://graph.facebook.com https://graph.instagram.com https://*.facebook.com; frame-src 'self' https://*.supabase.co;"
           }
         ]
       }
